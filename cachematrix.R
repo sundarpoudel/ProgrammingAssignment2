9b2059fdd9d1e993f80522c9d36c637c80bce608
sundarpoudel <sundarpoudel@gmail.com>
2016-02-07 11:48:32 -0800

    Programming assignment2. Complete functiions to retunr cached inverse for invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computes the inverse of an invertible
 
-## Write a short comment describing this function
 
+## Create a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+	minverse <- NULL
+        set <- function(y) {
+            x <<- y
+            minverse <<- NULL
+        }
+            get <- function() x
+            setinverse <- function(inverse) minverse <<- inverse
+            getinverse <- function() minverse
 
+            list(set = set, get = get,
+                 setinverse = setinverse,
+                 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## compute the inverse of the special "matrix" returned by `makeCacheMatrix` above. 
+## If the inverse has already been calculated (and the matrix has not changed), then
+##  `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		  minverse <- x$getinverse()
+            if(!is.null(minverse)) {
+                    message("getting cached data")
+                    return(minverse)
+            }
+            data <- x$get()
+            ## get he inverse
+            minverse <- solve(data, ...)
+            x$setinverse(minverse)
+            minverse
 }
